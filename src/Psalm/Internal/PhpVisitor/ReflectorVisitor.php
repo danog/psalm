61512da14450700a91166c2123bfd0002bdb0a16
orklah <orklah@users.noreply.github.com>
2020-09-04 22:26:33 +0200

    add native return types (#4116)
    * add native return types
    
    * remove redundant phpdoc

--- src/Psalm/Internal/PhpVisitor/ReflectorVisitor.php
+++ src/Psalm/Internal/PhpVisitor/ReflectorVisitor.php
@@ -162,7 +162,6 @@ class ReflectorVisitor extends PhpParser\NodeVisitorAbstract implements PhpParse
     }
 
     /**
-     * @param  PhpParser\Node $node
      *
      * @return null|int
      */
@@ -608,7 +607,7 @@ class ReflectorVisitor extends PhpParser\NodeVisitorAbstract implements PhpParse
             }
 
             $converted_aliases = \array_map(
-                function (TypeAlias\InlineTypeAlias $t) {
+                function (TypeAlias\InlineTypeAlias $t): ?TypeAlias\ClassTypeAlias {
                     try {
                         $union = TypeParser::parseTokens(
                             $t->replacement_tokens,
@@ -1817,7 +1816,6 @@ class ReflectorVisitor extends PhpParser\NodeVisitorAbstract implements PhpParse
     }
 
     /**
-     * @param  PhpParser\Node\FunctionLike $stmt
      * @param  bool $fake_method in the case of @method annotations we do something a little strange
      *
      * @return FunctionLikeStorage|false
@@ -2619,8 +2617,7 @@ class ReflectorVisitor extends PhpParser\NodeVisitorAbstract implements PhpParse
         if ($storage instanceof MethodStorage) {
             $storage->has_docblock_param_types = (bool) array_filter(
                 $storage->params,
-                /** @return bool */
-                function (FunctionLikeParameter $p) {
+                function (FunctionLikeParameter $p): bool {
                     return $p->type !== null && $p->has_docblock_type;
                 }
             );
@@ -3147,11 +3144,6 @@ class ReflectorVisitor extends PhpParser\NodeVisitorAbstract implements PhpParse
         return $assertion_type_parts;
     }
 
-    /**
-     * @param  PhpParser\Node\Param $param
-     *
-     * @return FunctionLikeParameter
-     */
     public function getTranslatedFunctionParam(
         PhpParser\Node\Param $param,
         PhpParser\Node\FunctionLike $stmt,
@@ -3296,9 +3288,7 @@ class ReflectorVisitor extends PhpParser\NodeVisitorAbstract implements PhpParse
     }
 
     /**
-     * @param  FunctionLikeStorage          $storage
      * @param  array<int, array{type:string,name:string,line_number:int,start:int,end:int}>  $docblock_params
-     * @param  PhpParser\Node\FunctionLike  $function
      *
      * @return void
      */
@@ -3518,8 +3508,6 @@ class ReflectorVisitor extends PhpParser\NodeVisitorAbstract implements PhpParse
     }
 
     /**
-     * @param   PhpParser\Node\Stmt\Property    $stmt
-     * @param   Config                          $config
      * @param   string                          $fq_classlike_name
      *
      * @return  void
@@ -3709,7 +3697,6 @@ class ReflectorVisitor extends PhpParser\NodeVisitorAbstract implements PhpParse
     }
 
     /**
-     * @param   PhpParser\Node\Stmt\ClassConst  $stmt
      * @param   string $fq_classlike_name
      *
      * @return  void
@@ -3989,7 +3976,6 @@ class ReflectorVisitor extends PhpParser\NodeVisitorAbstract implements PhpParse
     }
 
     /**
-     * @param  PhpParser\Node\Expr\Include_ $stmt
      *
      * @return void
      */
@@ -4050,42 +4036,27 @@ class ReflectorVisitor extends PhpParser\NodeVisitorAbstract implements PhpParse
         }
     }
 
-    /**
-     * @return string
-     */
-    public function getFilePath()
+    public function getFilePath(): string
     {
         return $this->file_path;
     }
 
-    /**
-     * @return string
-     */
-    public function getFileName()
+    public function getFileName(): string
     {
         return $this->file_scanner->getFileName();
     }
 
-    /**
-     * @return string
-     */
-    public function getRootFilePath()
+    public function getRootFilePath(): string
     {
         return $this->file_scanner->getRootFilePath();
     }
 
-    /**
-     * @return string
-     */
-    public function getRootFileName()
+    public function getRootFileName(): string
     {
         return $this->file_scanner->getRootFileName();
     }
 
-    /**
-     * @return Aliases
-     */
-    public function getAliases()
+    public function getAliases(): Aliases
     {
         return $this->aliases;
     }
