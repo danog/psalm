f62981c826dec8b7cdfc12484630de20aa06017e
Daniil Gentili <daniil@daniil.it>
2022-12-03 18:32:28 +0100

    Fix

--- src/Psalm/Type/Reconciler.php
+++ src/Psalm/Type/Reconciler.php
@@ -1141,6 +1141,7 @@ class Reconciler
                         $properties = $base_atomic_type->properties;
                         $properties[$array_key_offset] = $result_type;
                         if ($base_atomic_type->is_list
+                            && is_numeric($array_key_offset)
                             && $array_key_offset
                             && !isset($properties[$array_key_offset-1])
                         ) {
